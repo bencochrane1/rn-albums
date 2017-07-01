c27bed1346a8e5c24ea86266ff8084ed4e5eda2f
Sam Cochrane <Sam@Sams-MacBook-Air.local>
2017-07-01 21:32:05 +1000

    update for card item'

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+
+	const { title, artist, thumbnail_image } = album;
+
 	return (
 		<Card >
-			<Text>{props.album.title}</Text>
+			<CardSection>
+				<View>
+					<Image source={{uri: thumbnail_image}} />
+				</View>
+				<View style={styles.headerContentStyle}>
+					<Text>{title}</Text>
+					<Text>{artist}</Text>
+				</View>				
+			</CardSection>
 		</Card>
 	);
 };
+
+const styles = {
+	headerContentStyle: {
+		flexDirection: 'column',
+		justifyContent: 'space-around'
+	}
+}
 
 export default AlbumDetail;
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { View } from 'react-native';
 
 const Card = (props) => {
-	console.log('card props', props)
 	return (
 		<View style={styles.containerStyle}>
 			{props.children}
